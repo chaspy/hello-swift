c9785fb5b4a932c10a4ed17396245531aa1be814
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-02 18:18:09 +0900

    style: refactor ContentView to use VStack and add a Button

--- hello-swift/ContentView.swift
+++ hello-swift/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hi, Swift!")
-            .font(.largeTitle)
+        VStack {
+            Text("Hi, Swift!")
+                .font(.largeTitle)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
