d8b05eaef77903f05877fe1dc72e3996f0c1714c
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-02 17:50:37 +0900

    init

---
+++ hello-swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello-swift
+//
+//  Created by Takeshi Kondo on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
